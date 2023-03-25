89f476f1a15a435e92ab5043eb04b28d0c81357b
16132250 <jason.curate50@spsmail.cuny.edu>
2023-03-25 13:22:03 -0400

    basic url read with beautiful soup

--- apple_stock.py
+++ apple_stock.py
@@ -1,2 +1,15 @@
+import urllib.request
+from bs4 import BeautifulSoup
+url = "http://www.google.com"
+
+def get_data(url):
+    with urllib.request.urlopen(url) as response:
+        response = response.read().decode('utf-8')
+
+    return response
+
 if __name__ == "__main__":
-    pass
+    page = get_data(url)
+    soup = BeautifulSoup(page, 'html.parser')
+
+    print(soup.prettify())
